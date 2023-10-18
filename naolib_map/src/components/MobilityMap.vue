78c4895ec23e7fbe17bff4788d23a08a5c4f8319
AdelineMartineau <adeline.martineau@epsi.fr>
2023-10-18 16:05:19 +0200

    zoom on map

--- naolib_map/src/components/MobilityMap.vue
+++ naolib_map/src/components/MobilityMap.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="height:600px; width:900px">
-    <l-map ref="map" v-model:zoom="zoom" :center="[46.6, 2.19482]">
+    <l-map ref="map" v-model:zoom="zoom" :center="[47.216671, -1.55]">
       <l-tile-layer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         layer-type="base"
@@ -12,17 +12,16 @@
 
 <script>
 import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
 
 export default {
   components: {
     LMap,
     LTileLayer,
-    LMarker
   },
   data() {
     return {
-      zoom: 6,
+      zoom: 12,
     };
   },
 };
